c653094783b2827a9d94aa6e4e6d3f8b970d94af
Aaksh Ranjan <aakshranjan@outlook.com>
2024-02-19 22:23:55 -0800

    Bug Fix: repository_path is a variable

--- jenkins/config/setup.sh
+++ jenkins/config/setup.sh
@@ -2,6 +2,7 @@
 
 # Run the docker clean_up.sh script
 repository_path=$(git rev-parse --show-toplevel)
-. repository_path/jenkins/scripts/docker/clean_up.sh
+. ${repository_path}/jenkins/scripts/docker/clean_up.sh
 
+# Run the docker-compose up command, to bring up the Jenkins Server.
 docker-compose up -d
