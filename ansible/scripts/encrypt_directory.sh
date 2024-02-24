27285d7b138f441b7443561f6fc2d746b2f6e273
Aaksh Ranjan <aakshranjan@outlook.com>
2024-02-24 15:31:09 -0800

    Creating scripts to Encrypt Private Data

---
+++ ansible/scripts/encrypt_directory.sh
@@ -0,0 +1,33 @@
+#!/bin/zsh
+
+# source my zsh aliases.
+source ~/.zshrc
+
+# Check if the correct number of arguments are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <directory_path> <encrypted_file_path>"
+    exit 1
+fi
+
+# Assign arguments to variables
+directory_path=$1
+encrypted_file_path=$2
+
+# Set flag to track the first iteration
+first_iteration=true
+
+# Iterate over each file in the directory
+for file_path in "$directory_path"/*; do
+    # Extract the file name without the directory path
+    file_name=$(basename "$file_path")
+    # Extract the content of the file
+    file_content=$(cat "$file_path")
+    # Encrypt the content using ansible-vault
+    if [ "$first_iteration" = true ]; then
+        ansible-vault encrypt_string "$file_content" --name "$file_name" --vault-password-file ~/.ansible-vault.txt > "$encrypted_file_path"
+        # Set flag to false after the first iteration
+        first_iteration=false
+    else
+        ansible-vault encrypt_string "$file_content" --name "$file_name" --vault-password-file ~/.ansible-vault.txt >> "$encrypted_file_path"
+    fi
+done
---
+++ ansible/scripts/encrypt_file.sh
@@ -0,0 +1,24 @@
+
+#!/bin/zsh
+
+# source my zsh aliases.
+source ~/.zshrc
+
+# Check if the correct number of arguments are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <file_path> <encrypted_file_path>"
+    exit 1
+fi
+
+# Assign arguments to variables
+file_path=$1
+encrypted_file_path=$2
+
+# Extract the file name without the directory path
+file_name=$(basename "$file_path")
+
+# Extract the content of the file
+file_content=$(cat "$file_path")
+
+# Encrypt the content using ansible-vault and append it to the encrypted file
+ansible-vault encrypt_string "$file_content" --name "$file_name" --vault-password-file ~/.ansible-vault.txt > "$encrypted_file_path"
