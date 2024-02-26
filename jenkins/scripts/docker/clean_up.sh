64f742eb0cd77a083e331e2e6a4862a0bb4491f3
Aaksh Ranjan <aakshranjan@outlook.com>
2024-02-25 16:19:13 -0800

    Improve the cleanup script

--- jenkins/scripts/docker/clean_up.sh
+++ jenkins/scripts/docker/clean_up.sh
@@ -6,19 +6,14 @@ if [ "$(docker ps -q)" ]; then
     docker rm -f $(docker ps -aq)
 fi
 
+# Remove all dangling containers
+if [ "$(docker ps -aq -f status=exited)" ]; then
+    docker system prune -f
+fi
+
 # Remove all images
 if [ "$(docker images -q)" ]; then
     docker rmi -f $(docker images -aq)
-fi
-
-# Remove all volumes
-if [ "$(docker volume ls -q)" ]; then
-    docker volume rm $(docker volume ls -q)
-fi
-
-# Remove all networks
-if [ "$(docker network ls -q)" ]; then
-    docker network rm $(docker network ls -q)
 fi
 
 # Remove all dangling images
@@ -26,17 +21,22 @@ if [ "$(docker images -q -f dangling=true)" ]; then
     docker image prune -f
 fi
 
+# Remove all volumes
+if [ "$(docker volume ls -q)" ]; then
+    docker volume rm $(docker volume ls -q)
+fi
+
 # Remove all dangling volumes
 if [ "$(docker volume ls -q -f dangling=true)" ]; then
     docker volume prune -f
 fi
 
+# Remove all networks
+if [ "$(docker network ls -q)" ]; then
+    docker network rm $(docker network ls -q)
+fi
+
 # Remove all dangling networks
 if [ "$(docker network ls -q -f dangling=true)" ]; then
     docker network prune -f
-fi
-
-# Remove all dangling containers
-if [ "$(docker ps -aq -f status=exited)" ]; then
-    docker system prune -f
 fi
