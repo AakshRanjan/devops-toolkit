bce32b6f78da214ca0ab2fb2f306921073bbba30
Aaksh Ranjan <aakshranjan@outlook.com>
2024-02-25 16:04:19 -0800

    Using a .env file to remove some of the hardcoded code

--- jenkins/start-jenkins/start_jenkins.sh
+++ jenkins/start-jenkins/start_jenkins.sh
@@ -7,8 +7,8 @@ source .env
 . ${repository_path}/jenkins/scripts/docker/clean_up.sh
 
 # Create a directory to mount 
-mkdir -p ${jenkins_data_path}
-mkdir -p ${jenkins_certs_path}
+mkdir -p "${jenkins_data_path}"
+mkdir -p "${jenkins_certs_path}"
 
 # Run the docker-compose up command, to bring up the Jenkins Server.
 docker-compose up --build -d
